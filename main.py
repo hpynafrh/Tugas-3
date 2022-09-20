d9ced61811e6963396ddb26da6976e34c8bef149
hpynafrh <113113752+hpynafrh@users.noreply.github.com>
2022-09-20 12:41:26 +0700

    add mul and div

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math_function import add
+from math_function import add,mul,div
 
 
 def main():
@@ -9,10 +9,15 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":
+        result = mul(data_1, data_2)
+    elif operator == "/"
+        result = div(data_1, data_2)
 
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+    print("{} {} {} = {}").format(data_1,operator, data_2, result))
 
 
-if __name__ == "__main__":
+
+     if __name__ == "__main__":
     print("Hello Main !")
     main()
